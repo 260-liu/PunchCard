e8beab31d7ff72d7f7bf2e95bb589262da200f00
达 <2664639231@qq.com>
2020-05-24 22:39:58 +0800

    修改了NewrecordActivity的内容，和部署内容

--- PuchCard/app/src/main/java/com/example/punchcard/NewrecordActivity.java
+++ PuchCard/app/src/main/java/com/example/punchcard/NewrecordActivity.java
@@ -8,13 +8,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.punchcard.database.SQLiteHelper;
 import com.example.punchcard.utils.DBUtils;
 
 /**
  * 新建一个打卡项目的activity
  */
-public class NewrecordActivity extends Activity {
+public class NewrecordActivity extends AppCompatActivity {
 
     private static final String TAG = "NewrecordActivity" ;
     EditText editText;
